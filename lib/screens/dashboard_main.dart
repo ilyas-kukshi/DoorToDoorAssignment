96172b143ae1bf75fbd1196aeeb2b4b13d30e365
ilyas-kukshi <ilyaskukshiwala53@gmail.com>
2022-05-09 21:34:31 +0530

    intial setup

---
+++ lib/screens/dashboard_main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DashBoardMain extends StatefulWidget {
+  const DashBoardMain({ Key? key }) : super(key: key);
+
+  @override
+  State<DashBoardMain> createState() => _DashBoardMainState();
+}
+
+class _DashBoardMainState extends State<DashBoardMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
